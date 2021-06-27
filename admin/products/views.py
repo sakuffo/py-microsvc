dff1a3470d0490b60fe0d8a5a94c99e079a6be2e
Stephen Akuffo <sa.akuffo@gmail.com>
2021-06-27 13:06:28 -0400

    api working

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,18 +1,28 @@
 # from django.shortcuts import render
-from rest_framework import viewsets
+import imp
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
+from .models import Product
+from .serializers import ProductSerializer
 # tutorial-learning, this is looking like where you defined how the application interfaces with the db
 
 class ProductViewSet(viewsets.ViewSet):
   def list(self, request): # /api/products
-    pass
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
   def create(self, request): # /api/products
-    pass
+    serializer = ProductSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
 
   def retrieve(self, request, pk=None): # .api/products/<str:id>
-    pass
+    product = Product.objects.get(id=pk)
+    searlier
   def update(self, request, pk=None): # .api/products/<str:id>
     pass
-  def delete(self, request, pk=None): # .api/products/<str:id>
+  def destroy(self, request, pk=None): # .api/products/<str:id>
     pass
